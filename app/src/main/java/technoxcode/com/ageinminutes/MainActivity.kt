6baab1032ac5ece9eee5e8ef36ff021a50edbc7a
Configex <asimaftab303@gmail.com>
2022-09-27 22:29:33 +0545

    Initial Commit

--- app/src/main/java/technoxcode/com/ageinminutes/MainActivity.kt
+++ app/src/main/java/technoxcode/com/ageinminutes/MainActivity.kt
@@ -4,12 +4,11 @@ import android.app.DatePickerDialog
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Gravity
-import android.view.View
+
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import org.w3c.dom.Text
+
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -40,11 +39,12 @@ class MainActivity : AppCompatActivity() {
         val dpd = DatePickerDialog(
             this, { _, Selectedyear, Selectedmonth, SelecteddayofMonth ->
                 //  Toast.makeText(this, "Date picker works", Toast.LENGTH_LONG).show()
-               val mytoast = Toast.makeText(
+                val mytoast = Toast.makeText(
                     this,
                     "Selected Year was $Selectedyear, month was ${Selectedmonth + 1}, Day was $SelecteddayofMonth",
-                    Toast.LENGTH_LONG,);
-                mytoast.getView()?.setBackgroundColor((Color.parseColor("#F6AE2D")));mytoast.show()//It count month from 0 so we added 1 and made it
+                    Toast.LENGTH_LONG,)
+                mytoast.getView()
+                    ?.setBackgroundColor((Color.parseColor("#80ed99")));mytoast.show()//It count month from 0 so we added 1 and made it
 
                 val selecteddate = "$SelecteddayofMonth/${Selectedmonth + 1}/$Selectedyear"
                 selectedatetv?.setText(selecteddate)
@@ -53,14 +53,15 @@ class MainActivity : AppCompatActivity() {
                 thedate?.let {
                     val selectedDateinMinutes = thedate.time / 60000
                     val currentdate = sdf.parse(sdf.format(System.currentTimeMillis()))
-                    val selectedDateinhrs = thedate.time/ 60000/60
+                    val selectedDateinhrs = thedate.time / 60000 / 60
 
                     currentdate?.let {  //it will run code only when date is not empty
                         val currentdateinminutes = currentdate.time / 60000
-                        val currentdatehr = currentdate.time/60000/60
+                        val currentdatehr = currentdate.time / 60000 / 60
                         val differenceInminutes = currentdateinminutes - selectedDateinMinutes
                         val differenceinHrs = currentdatehr - selectedDateinhrs
-                        tvageinminutes?.text=("Hrs: $differenceinHrs hrs/ Minutes: $differenceInminutes min").toString()
+                        tvageinminutes?.text =
+                            ("Hrs: $differenceinHrs hrs/ Minutes: $differenceInminutes min").toString()
 
                     }
 
